66244a71bb97a03161e4d161c16591c31ddbd8a2
Eliezer Graber <me@eygraber.com>
2024-11-27 21:31:04 -0500

    Update conventions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -46,7 +46,7 @@ dependencyResolutionManagement {
 }
 
 plugins {
-  id("com.eygraber.conventions.settings") version "0.0.79"
+  id("com.eygraber.conventions.settings") version "0.0.81"
   id("com.gradle.develocity") version "3.18.2"
 }
 
